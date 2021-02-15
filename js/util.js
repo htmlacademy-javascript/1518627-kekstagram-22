a91d764b0b137113a8f219d4afc942c50c4d7d36
Bedmano <nike-22@list.ru>
2021-02-15 18:20:53 +0300

    Модуляция

---
+++ js/util.js
@@ -0,0 +1,22 @@
+// Функция для получения случайного числа взята с сайта https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+const getRandom = function (min, max) {
+  if (min > max) {
+    let swap = min;
+    min = max;
+    max = swap;
+  }
+
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const getCommentLength = function (currentLength, maxLength) {
+  if (currentLength <= maxLength) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export { getRandom, getCommentLength };
